d0c59db47d72521f4209384182a8b1f66e9dc6f2
Nombuso Mdletshe <nombusomnmdletshe@outlook.com>
2025-03-16 07:26:53 +0200

    Add project files.

---
+++ S1L05_RWD/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using S1L05_RWD.Models;
+using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S1L05_RWD.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            
+            return View();
+        }
+        public ActionResult Consultation()
+        {
+
+                return View();
+            
+        }
+
+    }
+}
